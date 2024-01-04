a5780f580eb556c4c462d16b488a8a7dffaac2c3
vaishuh2004 <149518010+vaishuh2004@users.noreply.github.com>
2024-01-04 15:44:08 +0530

    OS 6th(C)program

---
+++ first.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#define max 25
+void main()
+{
+	int
+		frag[max],b[max],f[max],i,j,nb,nf,temp,highest=0;
+        static int bf[max],ff[max];
+        printf("\n\tMempry Management Scheme -First fit");
+        printf("\nEnter the number of blocks:");
+        scanf("%d",&nb);
+        printf("enter the number of files:");
+        scanf("%d",&nf);
+        printf("\nenter the size of the blocks:-\n");
+        for(i=0;i<=nb;i++)
+        {
+                printf("Block %d:",i);
+                scanf("%d",&b[i]);
+        }
+        printf("Enter the size of the files:-\n");
+       for(i=0;i<=nf;i++)
+       {
+               printf("File %d:",i);
+               scanf("%d",&f[i]);
+       }
+       for(i=1;i<=nf;i++)
+       {
+               for(j=1;j<=nb;j++)
+               {
+                       if(bf[j]!=1)
+                       {
+                               temp=b[j]-f[i];
+                               if(temp>=0)
+				       if(highest<temp)
+				       {
+	       }
+       }
+       frag[i]=highest;
+       bf[ff[i]]=1;
+       highest=0;
+}
+ff[i]=j;
+highest=temp;
+}
+printf("\nFile_no:\tFile_size:\tBlock_no:\tBlock_size:\tFragement");
+       for(i=1;i<=nf;i++)
+               printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,f[i],ff[i],b[ff[i]],frag[i]);
+	       }
+	       
